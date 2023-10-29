8b92d300fcd8cce127f00e413dd92aa706fec721
Shredan Abdullah <105118008+shredanabdullah@users.noreply.github.com>
2023-10-29 21:48:43 +0200

    NOT COMPLETED #1.H. Two numbers.cpp

---
+++ #1.H. Two numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long x,y;
+    cin >> x>>y;
+    
+    long long floor,ceil,round;
+    
+    
+ 
+
+    cout << "floor " <<x<<"/"<<y<<" = "<<floor<< endl;
+    cout << "ceil " <<x<<"/"<<y<<" = "<<ceil<< endl;
+    cout << "round " <<x<<"/"<<y<<" = "<<round<< endl;
+    
+    return 0;
+}
