22bd5141b0549c2b9f01793f226bcd351735040c
Shredan Abdullah <105118008+shredanabdullah@users.noreply.github.com>
2023-10-30 15:38:25 +0200

    Create #1.I. Welcome for you with Conditions.cpp

---
+++ #1.I. Welcome for you with Conditions.cpp
@@ -0,0 +1,27 @@
+/*
+#include<iostream>
+using namespace std;
+int main(){
+      char x,y;///////////////ana 3mlt kda 3la asas elconstains eno x,y is <= 100 fa olt awafar msa7a BS hwa kda et3amel m3 x,y 3ala asas elascii code msh numric value so takecare
+      cin>>x>>y;
+      if(x>=y){
+         cout<<"Yes"; 
+      }
+      else{
+           cout<<"No";  
+      }
+      return 0;
+}*/
+#include<iostream>
+using namespace std;
+int main(){
+      int x,y;
+      cin>>x>>y;
+      if(x>=y){
+         cout<<"Yes"; 
+      }
+      else{
+           cout<<"No";  
+      }
+      return 0;
+}
