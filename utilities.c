76ebddb83df5578dc0aab82770b52ea9a930a4d4
Shredan Abdullah <105118008+shredanabdullah@users.noreply.github.com>
2024-03-07 18:52:21 +0200

    Update utilities.c

--- utilities.c
+++ utilities.c
@@ -9,3 +9,13 @@ int readString(char *str){
   str[i]='\0';
   return i;// Return the length of the string (excluding the null terminator)
 }
+/*
+int main() {
+  char input[100]; // Define a character array to store the input string
+  printf("Enter a string: ");
+  int length = readString(input); // Call the readString function to read the string
+  printf("You entered: %s\n", input); // Print the input string
+  printf("Length of the string: %d\n", length); // Print the length of the string
+  return 0;
+}
+*/
